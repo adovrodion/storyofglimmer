335da9ff72cdf103a02216be642e02f13df4c6fe
adovrodion <bizpromotionru@gmail.com>
2025-07-03 00:19:30 +0300

    feat: add MAX_SUPPLY cap and Minted event, drop ReentrancyGuard in Glimmer contract

--- src/contracts/NFTMint.sol
+++ src/contracts/NFTMint.sol
@@ -4,14 +4,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-contract Glimmer is ERC721URIStorage, Ownable, ReentrancyGuard {
+/// @title Glimmer NFT contract with capped supply
+/// @notice Simplified ERC-721 with single TOKEN_URI and pause + supply cap
+contract Glimmer is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
+    /// @dev Maximum amount of tokens that can ever be minted.
+    uint256 public constant MAX_SUPPLY = 5000;
+
     string public constant TOKEN_URI = "https://ipfs.filebase.io/ipfs/QmQwV4umGApbKUta37UUTUHVLAW8oPW2LsVLwArwR2zavD";
     bool public paused = false;
+
+    /// @notice Emitted each time a new NFT is minted.
+    event Minted(address indexed to, uint256 tokenId);
 
     constructor(address initialOwner) 
         ERC721("Glimmer", "GL") 
@@ -23,11 +30,14 @@ contract Glimmer is ERC721URIStorage, Ownable, ReentrancyGuard {
         _;
     }
 
-    function safeMint(address to) public whenNotPaused nonReentrant {
+    function safeMint(address to) public whenNotPaused {
+        require(totalSupply() < MAX_SUPPLY, "Max supply reached");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, TOKEN_URI);
+
+        emit Minted(to, tokenId);
     }
 
     function totalSupply() public view returns (uint256) {
