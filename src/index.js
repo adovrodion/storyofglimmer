7c94494d623ca7a3762c4ce69e7a3b7fd72928b2
adovrodion <bizpromotionru@gmail.com>
2025-07-03 00:16:00 +0300

    fix: silence React-Modal accessibility warning by setting app element

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Modal from 'react-modal';
 import App from './App';
+
+// remove react-modal accessibility warning
+Modal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
